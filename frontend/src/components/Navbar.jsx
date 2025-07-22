970e8f66000f0eecb2897d23acd880cab23e33c7
Satyam5206 <satyamsavita06@gmail.com>
2025-07-22 22:57:40 +0530

    NEWW

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                     <div className='flex items-center gap-4'>
                         <Link to={"/createPage"} className='btn btn-primary'>
                             <PlusIcon className='size-5' />
-                            <span>New Note</span>
+                            <span>New note</span>
                         </Link>
                     </div>
                 </div>
